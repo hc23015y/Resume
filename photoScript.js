0aa48394be4ec07a8b3c9ecba42c0a778b316d08
Fank Yang <fank@Macde-MacBook-Pro.local>
2016-10-14 03:51:12 +0800

    upload photoScript.js

---
+++ photoScript.js
@@ -0,0 +1,6 @@
+var imgTag = document.createElement("img");
+$("#wmd-preview").append(imgTag);
+imgTag.src = "XXXX";
+imgTag.style.position = "absolute";
+imgTag.style.width = "150px";
+imgTag.style.top = "20px";
